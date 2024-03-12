99bb537009b4abc962ffda14029840adbf6555f6
alanfavatier <alanfavatier305@gmail.com>
2024-03-12 15:54:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors=require("cors")
+const routerApi = require("./routes");
+
+const {logErrors,errorHandler,boomErrorHandler }=require("./middlewares/error.handler")
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+const whitelist= ["http://localhost:8080", "http://myapp.com"];
+const options={
+  origin:(origin , callback)=>{
+if (whitelist.includes(origin)) {
+  callback(null,true)
+}else{
+  callback(new Error("no permitido"))
+}
+  }
+}
+app.use(cors(options))
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hola");
+});
+
+routerApi(app);
+
+app.use(logErrors)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log("Servidor corriendo en " + port);
+});
