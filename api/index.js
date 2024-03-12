32e99f344cb6984b7c5de8320cea0e26cfe08771
alanfavatier <alanfavatier305@gmail.com>
2024-03-12 16:58:39 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -5,15 +5,15 @@ const routerApi = require("./routes");
 const {logErrors,errorHandler,boomErrorHandler }=require("./middlewares/error.handler")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 
-const whitelist= ["http://localhost:8080", "http://myapp.com"];
+ const whitelist= ["http://localhost:3000", "http://myapp.com"];
 const options={
   origin:(origin , callback)=>{
-if (whitelist.includes(origin)) {
+if (whitelist.includes(origin) || !origin) {
   callback(null,true)
 }else{
   callback(new Error("no permitido"))
@@ -24,7 +24,8 @@ app.use(cors(options))
 
 
 
-app.get("/", (req, res) => {
+
+app.get("/api", (req, res) => {
   res.send("Hola");
 });
 
